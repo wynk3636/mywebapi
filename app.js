cfdaf1c27a93bdc9457d5cd8b8ce89066cbb0a23
ishida <h_hand_3636@gmail.com>
2019-09-03 21:53:35 +1000

    CRS

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ const app = express();
 
 var port = process.env.PORT || 8080;
 
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+});
+
 app.get('/',(req,res) => res.send('Hellow'));
 
 app.get('/api',(req,res) => {
