81cffb54721ada33bd7834ae83072827f83b7c06
ishida <h_hand_3636@gmail.com>
2019-09-03 20:25:59 +1000

    test

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ app.get('/',(req,res) => res.send('Hellow'));
 app.get('/api',(req,res) => {
 	const todoList = [
 		{title:'aaa',done:true},
-		{title:'bbb',done:false}
+		{title:'bbb',done:false},
+		{title:'ccc',done:false}
 	]
 	res.json(todoList);
 });
