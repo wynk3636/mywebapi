d302bed82ea8798d53823d581984f1c14c7b1876
ishida <h_hand_3636@gmail.com>
2019-09-03 23:46:02 +1000

    todoItem add

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const multer = require('multer');
+const uuidv4 = require('uuid/v4');
 
 const app = express();
+app.use(multer().none());
 
 var port = process.env.PORT || 8080;
+const todoList = [];
 
 app.use(function(req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");
@@ -12,13 +16,34 @@ app.use(function(req, res, next) {
 
 app.get('/',(req,res) => res.send('Hellow'));
 
-app.get('/api',(req,res) => {
-	const todoList = [
+app.get('/api/get',(req,res) => {
+	/*
+	todoList = [
 		{title:'aaa',done:true},
 		{title:'bbb',done:false},
 		{title:'ccc',done:false}
 	]
+	*/
 	res.json(todoList);
+});
+
+app.post('/api/post',(req,res) => {
+	const todoData = req.body;
+	const todoTitle = todoData.title;
+
+	const id = uuidv4();
+
+	const todoItem = {
+		id,
+		title:todoTitle,
+		done:false
+	};
+
+	todoList.push(todoItem);
+
+	console.log(JSON.stringify(todoItem));
+
+	res.json(todoItem);
 });
 
 app.listen(
