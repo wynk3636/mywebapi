80c05f377a0eb05392b5530a709ee1cc932e7949
ishida <h_hand_3636@gmail.com>
2019-09-05 22:56:16 +1000

    method

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const todoList = [];
 
 app.use(function(req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET", "PUT", "POST", "DELETE", "OPTIONS");
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 	next();
 });
